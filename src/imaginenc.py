4056c373e1011229108252d86066f8fa80955480
martinkozle <martinkozle@yahoo.com>
2021-08-22 07:57:02 +0200

    Add meta data to image

--- src/imaginenc.py
+++ src/imaginenc.py
@@ -3,23 +3,39 @@
 import math
 import sys
 import argparse
-from typing import List, Optional, Dict, Iterable
+from typing import List, Optional, Dict, Iterable, Union, Any
 
 import numpy as np
 from PIL import Image, ImageColor
 
+META_SIZE = 6
+META_INFO = {
+    'meta_size': (0, 3, int),
+    'extra_zeros': (3, 4, int)
+}
 
-def rgb_to_hex(r: np.uint8, g: np.uint8, b: np.uint8) -> str:
-    return f'{r:02x}{g:02x}{b:02x}'
+
+def parse_meta(data: bytes) -> Dict[str, Any]:
+    bytes_converters = {
+        int: bytes_to_int,
+        str: bytes_to_str
+    }
+    meta = {}
+    for key, (fr, to, type_) in META_INFO.items():
+        meta[key] = bytes_converters[type_](data[fr: to])
+    return meta
 
 
-def decode_image_to_bytes(image: Iterable[np.uint8]) -> bytes:
-    data = np.asarray(image, dtype=np.uint8)
-    file_data = []
-    for row in data:
-        for pixel in row:
-            file_data.append(rgb_to_hex(pixel[0], pixel[1], pixel[2]))
-    return bytes.fromhex(''.join(file_data))
+def decode_image_to_bytes(
+        image: Union[Iterable[np.uint8], Image.Image]) -> bytes:
+    data = bytes(list(np.asarray(image, dtype=np.uint8).flatten()))
+    meta = parse_meta(data)
+    file_data_hex = ''.join(
+        f'{pixel:02x}' for pixel in data[meta['meta_size']:]
+    )
+    return bytes.fromhex(
+        file_data_hex[:len(file_data_hex) - meta['extra_zeros'] * 2]
+    )
 
 
 def decode_image_name(input_file_name: str, output_file_name: str):
@@ -35,18 +51,39 @@ def decode_image_name(input_file_name: str, output_file_name: str):
         f.write(file_bytes)
 
 
-def get_file_bytes(input_file_name: str) -> Optional[List[bytes]]:
-    file_bytes = []
+def get_file_bytes(input_file_name: str) -> Optional[bytes]:
     try:
         with open(input_file_name, 'rb') as f:
-            while byte := f.read(1):
-                file_bytes.append(byte)
-        return file_bytes
+            return f.read()
     except OSError:
         return None
 
 
-def write_colors_to_image(colors: List[str], output_file_name: str):
+def int_to_bytes(num: int, num_bytes: int, signed: bool = False) -> bytes:
+    return num.to_bytes(num_bytes, byteorder='big', signed=signed)
+
+
+def bytes_to_int(bytes_: bytes, signed: bool = False) -> int:
+    return int.from_bytes(bytes_, 'big', signed=signed)
+
+
+def str_to_bytes(string: str) -> bytes:
+    return string.encode('ASCII')
+
+
+def bytes_to_str(bytes_: bytes) -> str:
+    return bytes_.decode('ASCII')
+
+
+def bytes_to_hex(bytes_: bytes) -> List[str]:
+    return list(map(lambda b: f'{b:02x}', bytes_))
+
+
+def int_to_n_hex(num: int, num_hex: int) -> List[str]:
+    return bytes_to_hex(int_to_bytes(num, num_hex))
+
+
+def colors_to_image(colors: List[str]) -> Image.Image:
     size = len(colors)
     root = math.ceil(math.sqrt(size))
     x = 1
@@ -62,16 +99,21 @@ def write_colors_to_image(colors: List[str], output_file_name: str):
             image_data.append(image_row)
             image_row = []
     image = Image.fromarray((np.array(image_data)).astype(np.uint8))
-    if not output_file_name.endswith('.png'):
-        output_file_name += '.png'
-    image.save(output_file_name)
+    return image
 
 
-def encode_bytes_to_colors(input_file_bytes: List[bytes]) -> List[str]:
+def encode_bytes_to_colors(input_file_bytes: bytes) -> List[str]:
+    input_file_hex = bytes_to_hex(input_file_bytes)
+    extra_zeros = -len(input_file_hex) % 3
+    meta_hex = [
+        *int_to_n_hex(META_SIZE, 3),
+        *int_to_n_hex(extra_zeros, 1)
+    ]
+    meta_hex += int_to_n_hex(0, META_SIZE - len(meta_hex))
     colors = []
     color = '#'
-    for byte in input_file_bytes:
-        color += byte.hex()
+    for byte_hex in meta_hex + input_file_hex:
+        color += byte_hex
         if len(color) == 7:
             colors.append(color)
             color = '#'
@@ -88,7 +130,10 @@ def encode_file_name(input_file_name: str, output_file_name: str):
         print('Invalid file.')
         return
     colors = encode_bytes_to_colors(input_file_bytes)
-    write_colors_to_image(colors, output_file_name)
+    image = colors_to_image(colors)
+    if not output_file_name.endswith('.png'):
+        output_file_name += '.png'
+    image.save(output_file_name)
 
 
 def parse_args() -> argparse.Namespace:
